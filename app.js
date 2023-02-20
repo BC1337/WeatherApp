e97235f63907bd7aca5f95ae9b6014046fe0667e
BC <94580108+BC1337@users.noreply.github.com>
2023-02-20 16:59:53 -0600

    added ability to change from celcius to farenheit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ window.addEventListener('load', () => {
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
+    let temperatureSection = document.querySelector('.temperature');
+    let temperatureSpan = document.querySelector('.temperature-span');
     let locationTimezone = document.querySelector('.location-timezone');
     let humidityLevel = document.querySelector('.humidity-level');
     let windSpeed = document.querySelector('.wind-section');
@@ -16,7 +18,7 @@ window.addEventListener('load', () => {
             const API_KEY = `5e5a5506b41c63f1169fc55236d4e0da`;
             const proxy = `https://cors-anywhere.herokuapp.com/`;
             const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
-            
+        
             fetch(api)
             .then(response => {
                 return response.json();
@@ -34,8 +36,20 @@ window.addEventListener('load', () => {
                 locationTimezone.textContent = name;
                 windSpeed.textContent = `Wind: ${degToCompass(deg)} at ${speed} Km/Hr `;
                 locationIcon.innerHTML = `<img alt='icon' src="icons/${icon}.png">`;
-
                 
+                // change temperature to Celcius / Farenheit
+                let tempConversion = (temp * 1.8) + 32;
+
+                temperatureSection.addEventListener('click', () => {
+                    if (temperatureSpan.textContent === "C") {
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = Math.floor(tempConversion)
+                    } else {
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = temp;
+                    }
+                })
+                // change API data for wind from degrees to compass directions
                 function degToCompass(deg) {
                     var val = Math.floor((deg / 22.5) + 0.5);
                     var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
